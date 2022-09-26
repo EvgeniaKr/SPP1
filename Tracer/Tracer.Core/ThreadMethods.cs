639376271a3fb9f644d82e95814c864635b262f9
Home <evgeniakrig@gmail.com>
2022-09-27 01:02:00 +0300

    Add classes counting methods in a thread

---
+++ Tracer/Tracer.Core/ThreadMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer.Core
+{
+    public class ThreadMethods
+    {
+         public string Class { get; set; }
+        
+        public string Method { get; set; }
+        
+        public double Time { get; set; }
+
+        
+        public List<ThreadMethods> TThreadMethods { get; } //содержит список вложенных в него методов
+
+        public ThreadMethods(double time, string className, string methodName)
+        {
+            Time = time;
+            Class = className;
+            Method = methodName;
+            TThreadMethods = new List<ThreadMethods>();
+        }
+
+        public ThreadMethods()
+        {
+        }
+
+
+        public void AddThreadMethods(ThreadMethods method)
+        {
+            TThreadMethods.Add(method);
+        }
+    }
+}
---
+++ Tracer/Tracer.Core/TracerThread.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tracer.Core
+{
+    public class TracerThread
+    {
+        public int TId { get; set; }//идентификатор потока
+
+        public double Time//время исполнения потока
+        {
+            get
+            {
+                double time = 0;
+                foreach (var methodTraceResult in Methods)
+                {
+                    time += methodTraceResult.Time;
+                }
+                return time;
+            }
+            set
+            {
+
+            }
+        }
+        public List<ThreadMethods> Methods { get; }
+
+
+        public TracerThread()
+        {
+        }
+
+        public TracerThread(int Id)
+        {
+            TId = Id;
+            Methods = new List<ThreadMethods>();
+        }
+        public void AddMethod(ThreadMethods method)
+        {
+            Methods.Add(method);
+        }
+
+    }
+}
